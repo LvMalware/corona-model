f80540c0d5f84abb19510d7278e67d691b36c523
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2020-04-09 09:41:05 -0300

    added command line options and moved the estimation function to Corona.pm

--- Corona.pm
+++ Corona.pm
@@ -8,7 +8,38 @@ use Corona::Model;
 use base 'Exporter';
 
 our $VERSION = 0.3.2;
+our @EXPORT  = qw(estimate_days);
 
+sub estimate_days
+{
+    my %args = @_;
+    my $filename = $args{filename};
+    my $max_day  = $args{final} || die "No final day specified. Try passing 'final => day'";
+    my $w_p      = $args{wp} || 1;
+    my $w_e      = $args{we} || 1;
+    die "Invalid file: '$filename'" unless (-f $filename);
+    my $predictor = Corona::Model->new(file => $filename);
+    my %estimatives;
+    $predictor->load_file();
+    for my $day (1 .. $max_day)
+    {
+        $predictor->perform(($day < 28) ? 27 : $day - 1);
+        my $true_value = $predictor->{data}->{$day} || undef;
+        my $estimated  = ceil($predictor->eval($day, $w_e, $w_p));
+        my $abs_error  = defined($true_value) ? abs($true_value - $estimated) : undef;
+        my $rel_error  = defined($abs_error) ? 100 * $abs_error / $true_value : undef;
+        $estimatives{$day} = {
+            real      => $true_value,
+            estimated => $estimated,
+            abs_error => $abs_error,
+            rel_error => $rel_error,
+            po_weight => $w_p,
+            ex_weight => $w_e
+        };
+        $w_p ++ if (($day > 27) && defined($rel_error) && (int($rel_error) <= 1));
 
+    }
+    \%estimatives
+}
 
 1;
--- model.pl
+++ model.pl
@@ -1,26 +1,95 @@
 #!/usr/bin/env perl
 
-use POSIX;
 use strict;
 use lib '.';
 use warnings;
-use Corona::Model;
+use Getopt::Long;
+use Corona;
+use Data::Dump;
 
-my $m = Corona::Model->new(file => "dados.txt");
-$m->load_file();
-my ($p, $e) = (1, 1);
-print "DAY\tCASES\tESTIM\tERROR\tERROR (%)\n";
-for my $x (1 .. 44)
+sub help
 {
-    $m->perform(($x > 27) ? $x - 1 : 27);
-    my $y = ceil($m->eval($x, $e, $p));
-    my $rv = $m->{data}->{$x} || '-';
-    my $ea = ($rv ne '-') ? abs($y - $rv) : '-';
-    my $er = ($ea eq '-') ? $ea : 100 * $ea / $rv;
-    if ($x > 31)
-    {
-        $p ++ if (($er ne '-') && (int($er) <= 1));
-    }
-    printf("%d\t%s\t%d\t%s\t%s\n", $x, $rv, $y, $ea, $er);
+    print <<HELP;
+
+A numerical approach to model the growth of Corona Virus cases in Brazil
+
+Usage: $0 [options] <days>
+
+Options:
+
+    -h, --help              Display this help message and exit
+    -v, --version           Display the version and exit
+    -s, --start-day  SDAY   Display an interval of days starting with SDAY
+    -f, --final-day  FDAY   Display until FDAY
+    -i, --input-file FILE   Load data from FILE (defaults to 'data.txt')
+    -e, --error-stats       Display extra info about error on the model
+
+Examples:
+
+    $0 -s 34 -f 38 -e
+    $0 -i updated_data.txt 50 51
+
+Notes:
+        As with any numerical model, it finds only an approximation for future
+    values based on data from past events, without taking into account changes
+    in growth rates and/or the decrease in the number of cases. The results
+    obtained from this model cannot be taken as completely certain or immutable,
+    but only as estimates for the growth of the infection.
+        In addition, as the prevention measures adopted by the population come
+    into effect, reducing the growth in the number of cases, this model will
+    tend to become less accurate over time.
+
+Author:
+
+    Lucas V. Araujo <lucas.vieira.ar\@disroot.org>
+    GitHub: https://github.com/LvMalware
+
+HELP
+
+    exit(0)
 }
 
+sub version
+{
+    print "v0.3.2\n";
+    exit(0)
+}
+
+my $start_day  = 1;
+my $final_day  = undef;
+my $error_rep  = 0;
+my $input_file = 'data.txt';
+
+GetOptions(
+    "help"         => \&help,
+    "version"      => \&version,
+    "start-day=i"  => \$start_day,
+    "final-day=i"  => \$final_day,
+    "input-file=s" => \$input_file,
+    "error-stats"  => \$error_rep,
+);
+
+my @days = defined($final_day) ? $start_day .. $final_day : @ARGV;
+
+unless (@days > 0)
+{
+    print "Usage: $0 [options] <days>\n";
+    print "Try --help for more information.\n";
+    exit(0)
+}
+my @sorted = sort { $a <=> $b } @days;
+my $estimatives = estimate_days(filename => $input_file, final => $sorted[-1]);
+
+print "DAY\tCASES\tESTIM" . (($error_rep) ? "\tERROR\tERROR (%)\n" : "\n");
+
+for my $day (@days)
+{
+    printf("%d\t%s\t%d",  $day, $estimatives->{$day}->{real} || '-', $estimatives->{$day}->{estimated});
+    if ($error_rep)
+    {
+        my $abs_err = $estimatives->{$day}->{abs_error};
+        my $rel_err = $estimatives->{$day}->{rel_error};
+        printf("\t%s\t%.3s",  defined($abs_err) ? $abs_err : '-',  defined($rel_err) ? $rel_err : '-');
+    }
+    print "\n";
+}

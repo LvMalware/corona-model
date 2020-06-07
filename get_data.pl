e64d5a664a6481f0f98235cb79d2d38f41112d8e
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2020-06-07 09:07:24 -0300

    changed get_data.pl -- added new database

--- get_data.pl
+++ get_data.pl
@@ -5,15 +5,16 @@ use strict;
 use warnings;
 use HTTP::Tiny;
 
-my $request = HTTP::Tiny->new()->get('https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalCasos');
+my $request = HTTP::Tiny->new()->get('https://www.coronavirus.com.br/wp-content/uploads/c19/data.json?_t=1591530776');
+#https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalCasos
 if ($request->{success})
 {
     my $data = JSON::from_json($request->{content});
     open my $out, ">", "brasil.txt";
-    my $days = $data->{dias};
+    my $days = $data->{countries}->{Brazil}->{dates};
     for my $day (0 .. @$days - 1)
     {
-        print $out ($day + 1) . "        " . $days->[$day]->{'casosAcumulado'} . "\n";
+        print $out ($day + 1) . "        " . $days->[$day]->{'confirmed'} . "\n";
     }
     close $out;
 }
